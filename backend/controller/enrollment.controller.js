9074e4ab2ad688a85104c79f38db230ec2c627ac
AnojPeiris <it21161056@my.sliit.lk>
2024-03-23 11:05:24 +0530

    enrollment controller functions

--- backend/controller/enrollment.controller.js
+++ backend/controller/enrollment.controller.js
@@ -1,35 +1,61 @@
-import Enrollment from "../models/enrollmentModel.js";
+import Enrollment from "../models/enrollment.model.js";
 import { tryCatch } from "../utils/tryCatchWrapper.js";
+
+import { CustomError } from "../exceptions/baseException.js";
 
 // Controller for enrolling a user in a course
 const enrollUser = tryCatch(async (req, res) => {
   const { userID, courseID } = req.body;
-  const newEnrollment = new Enrollment({
+
+  if (!userID || !courseID)
+    throw new CustomError("All fields are required!", 500);
+
+  const isAlreadyEnrolled = await Enrollment.findOne({ userID, courseID });
+
+  if (isAlreadyEnrolled) throw new CustomError("Already enrolled!", 403);
+
+  const newEnrollment = await Enrollment.create({
     userID,
     courseID,
   });
-  const savedEnrollment = await newEnrollment.save();
-  res.status(201).json(savedEnrollment);
+
+  if (!newEnrollment) throw new CustomError("Enrollment failed", 500);
+
+  res.status(201).json(newEnrollment);
 });
 
 // Controller for unenrolling a user from a course
 const unenrollUser = tryCatch(async (req, res) => {
-  const { userID, courseunenrollUserID } = req.body;
+  const { userID, courseID } = req.body;
   const enrollment = await Enrollment.findOneAndDelete({ userID, courseID });
 
-  if (!enrollment) {
-    return res.status(404).json({ message: "Enrollment not found" });
-  }
+  if (!enrollment) throw new CustomError("Enrollment not found", 404);
 
-  res.json({ message: "User unenrolled from the course" });
-
-  res.status(500).json({ error: error.message });
+  res.status(200).json({ message: "User unenrolled from the course" });
 });
 
 // Controller for getting all enrollments
 const getAllEnrollments = tryCatch(async (req, res) => {
   const enrollments = await Enrollment.find();
+
+  if (!enrollments.length) throw new CustomError("No any enrollments", 404);
+
   res.json(enrollments);
 });
 
-export { enrollUser, unenrollUser, getAllEnrollments };
+const getEnrollmentByID = tryCatch(async (req, res) => {
+  const { userID, courseID } = req.body;
+
+  console.log("hello");
+
+  if (!userID || !courseID)
+    throw new CustomError("All fields are required!", 500);
+
+  const enrollment = await Enrollment.findOne({ userID, courseID });
+
+  if (!enrollment) throw new CustomError("Enrollment not found!", 404);
+
+  res.json(enrollment);
+});
+
+export { enrollUser, unenrollUser, getAllEnrollments, getEnrollmentByID };
